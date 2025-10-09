afcdbd23d963ea83642f554022eb744b6440c522
Aspen Price <aspen@aspen-otr.com>
2025-10-08 22:37:45 -0500

    Doom Tokyo Night Storm

--- config.el
+++ config.el
@@ -5,12 +5,12 @@
 
 (setq github-username "aspen-otr")
 
-(setq apotr/font-size 22)
-(setq doom-font (font-spec :family "0xProto Nerd Font" :size apotr/font-size)
-      doom-big-font (font-spec :family "0xProto Nerd Font" :size (* 1.25 apotr/font-size))
+(setq apotr/font-size 24)
+(setq doom-font (font-spec :family "RecMonoCasual Nerd Font" :size apotr/font-size)
+      doom-big-font (font-spec :family "RecMonoCasual Nerd Font" :size (* 1.25 apotr/font-size))
       doom-variable-pitch-font (font-spec :family "Arimo Nerd Font" :size apotr/font-size)
       doom-symbol-font (font-spec :family "Symbols Nerd Font" :size apotr/font-size))
 
-(setq doom-theme 'doom-ayu-mirage)
+(setq doom-theme 'doom-tokyo-night-storm)
 
 (setq display-line-numbers-type 't)
